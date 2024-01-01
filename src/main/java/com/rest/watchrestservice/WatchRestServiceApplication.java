49a52f9240abbfc0b129b2262043893783409f1d
mohamedehab00 <mohamed.ehab31450@gmail.com>
2024-01-01 14:31:00 +0200

    Mo Ehab Committed

---
+++ src/main/java/com/rest/watchrestservice/WatchRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rest.watchrestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchRestServiceApplication.class, args);
+    }
+
+}
